8261f2ce47aab509a31d0e80f174257d73dc7e22
xuan giang <ngxuangiang2020@gmail.com>
2022-02-07 21:28:15 +0700

    This is first lesson about Java Swing

--- src/Lession1/HelloWorld.java
+++ src/Lession1/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
         JFrame jFrame = new JFrame("First Programming with Java");
         jFrame.setSize(400, 400);
         jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
