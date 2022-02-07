f69f064d19d3a48fa07142ff7ee7e5d67e31b73f
xuan giang <ngxuangiang2020@gmail.com>
2022-02-07 21:24:58 +0700

    This is first lession about Java Swing

---
+++ src/Lession1/HelloWorld.java
@@ -0,0 +1,16 @@
+package Lession1;
+
+import javax.swing.*;
+
+public class HelloWorld {
+    public static void firstProgram()
+    {
+        JFrame jFrame = new JFrame("First Programming with Java");
+        jFrame.setSize(400, 400);
+        jFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        firstProgram();
+    }
+}
